660d914ce04f56bad02abf5498b85b85b61d38d6
malmquist <adam.malmquist@gmail.com>
2018-12-01 15:05:47 +0100

    add day1 a & b

---
+++ D01a/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace D01a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            foreach (var line in File.ReadAllLines("input.txt"))
+                sum += int.Parse(line);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ D01b/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace D01b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetRepeatingFrequency());
+        }
+
+        private static int GetRepeatingFrequency()
+        {
+            int sum = 0;
+            var frequencies = new List<int>();
+            var lines = File.ReadAllLines("input.txt");
+
+            while (true)
+            {
+                foreach (var line in lines)
+                {
+                    sum += int.Parse(line);
+                    if (!frequencies.Contains(sum))
+                    {
+                        frequencies.Add(sum);
+                    }
+                    else
+                    {
+                        return sum;
+                    }
+                }
+            }
+        }
+    }
+}
