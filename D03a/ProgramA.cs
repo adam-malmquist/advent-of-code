e291f4616dbafa5796c3264c283d0daa9eadac2a
malmquist <adam.malmquist@gmail.com>
2018-12-03 22:05:10 +0100

    day3

---
+++ D03a/ProgramA.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace D03a
+{
+    static class ProgramA
+    {
+        static void Main2()
+        {
+            var matrix = new byte[1000, 1000];
+            var regex = new Regex(@"#\d+ @ (\d+),(\d+): (\d+)x(\d+)");
+            var result = 0;
+
+            foreach (var line in File.ReadAllLines("input.txt"))
+            {
+                var match = regex.Match(line);
+                if (match.Success)
+                {
+                    int left = int.Parse(match.Groups[1].Value);
+                    int top = int.Parse(match.Groups[2].Value);
+                    int width = int.Parse(match.Groups[3].Value);
+                    int height = int.Parse(match.Groups[4].Value);
+
+                    for (int y = top; y < top + height; ++y)
+                        for (int x = left; x < left + width; ++x)
+                            if (++matrix[x, y] == 2)
+                                ++result;
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ D03a/ProgramB.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace D03a
+{
+    static class ProgramB
+    {
+        static void Main()
+        {
+            var claimedBy = new int[1000, 1000];
+            var intactClaims = new HashSet<int>();
+            var regex = new Regex(@"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)");
+
+            foreach (var line in File.ReadAllLines("input.txt"))
+            {
+                var match = regex.Match(line);
+
+                if (!match.Success)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                int id = int.Parse(match.Groups[1].Value);
+                int left = int.Parse(match.Groups[2].Value);
+                int top = int.Parse(match.Groups[3].Value);
+                int width = int.Parse(match.Groups[4].Value);
+                int height = int.Parse(match.Groups[5].Value);
+
+                bool conflict = false;
+
+                for (int y = top; y < top + height; ++y)
+                {
+                    for (int x = left; x < left + width; ++x)
+                    {
+                        if (claimedBy[x, y] == 0)
+                        {
+                            claimedBy[x, y] = id;
+                        }
+                        else
+                        {
+                            intactClaims.Remove(claimedBy[x, y]);
+                            conflict = true;
+                        }
+                    }
+                }
+
+                if (!conflict)
+                {
+                    intactClaims.Add(id);
+                }
+            }
+
+            Console.WriteLine(intactClaims.Single());
+        }
+    }
+}
