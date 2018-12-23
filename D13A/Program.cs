528398acbc5b6e1726cd1e73e25c0781dde4950a
malmquist <adam.malmquist@gmail.com>
2018-12-23 16:03:52 +0100

    D13A not started

---
+++ D13A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace D13A
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            foreach (var line in input)
+                Console.WriteLine(line);
+        }
+    }
+}
